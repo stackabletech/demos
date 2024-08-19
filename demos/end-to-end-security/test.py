c4c266637d4766bb437ba0becbf109598a3b1ef4
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2024-08-19 13:19:59 +0200

    WIP, bug reproduced

---
+++ demos/end-to-end-security/test.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, LongType, ShortType, FloatType, DoubleType, BooleanType, TimestampType, MapType, ArrayType
+from pyspark.sql.functions import col, from_json, expr
+
+import time
+
+spark = SparkSession.builder.appName("spark-report").getOrCreate()
+
+spark.sql("show catalogs").show()
+spark.sql("show tables in lakehouse.default").show()
+
+customer_table = "lakehouse.customer_analytics.customer"
+while not spark.catalog.tableExists(customer_table):
+    print(f"Table {customer_table} not found, waiting for Trino to create it...")
+    time.sleep(5)
+
+print(f"Table {customer_table} found, starting report")
+
+spark.sql(f"SELECT * FROM lakehouse.customer_analytics.customer").show()
+
+print("Job done")
