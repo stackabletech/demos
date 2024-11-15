add3a9a97a4a37a4d88e9900198902037f0d68b3
Nick Larsen <nick.larsen@stackable.tech>
2024-11-15 23:37:57 +0100

    feat: add update_refs.sh script

---
+++ _scripts/update_refs.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Ensure we are not on the `main` branch.
+if [[ "$CURRENT_BRANCH" == "main" ]]; then
+    >&2 echo "Will not replace github references for the main branch. Exiting."
+    exit 1
+fi
+
+# Ensure the index is clean
+if ! git diff-index --quiet HEAD --; then
+  >&2 echo "Dirty git index. Check working tree or staged changes. Exiting."
+  exit 2
+fi
+
+# Warn if the branch isn't a `release-*` branch (these changes shouldn't be in a branch that gets merged to main)
+if [[ "$CURRENT_BRANCH" != release-* ]]; then
+    >&2 echo "WARNING: doesn't look like a release branch. Continuing anyway."
+fi
+
+# Search for githubusercontent urls and replace the branch reference with a placeholder variable
+# This is done just in case the branch has special regex characters (like `/`).
+# shellcheck disable=SC2016 # We intentionally don't want to expand the variable.
+find demos stacks -type f \
+  -exec grep -l githubusercontent {} \; \
+  -exec sed -i -E 's/(stackabletech\/demos)\/main\//\1\/\${UPDATE_BRANCH_REF}\//' {} \;
+
+# Now, for all modified files, we can use envsubst
+export UPDATE_BRANCH_REF="$CURRENT_BRANCH"
+for MODIFIED_FILE in $(git diff --name-only); do
+  # shellcheck disable=SC2016 # We intentionally don't want to expand the variable.
+  envsubst '$UPDATE_BRANCH_REF' < "$MODIFIED_FILE" > "$MODIFIED_FILE.replacements"
+  mv "$MODIFIED_FILE.replacements" "$MODIFIED_FILE"
+done
