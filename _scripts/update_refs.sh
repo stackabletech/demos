500f07c4c042a743e0578c36a026f3228e1e5e7f
Nick Larsen <nick.larsen@stackable.tech>
2024-11-16 00:36:40 +0100

    feat: update update_refs.sh script to handle stackableRelease

--- _scripts/update_refs.sh
+++ _scripts/update_refs.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# This script is used by the stackable-utils release script to update the demos
+# repository branch references as well as the stackableRelease versions so that
+# demos are properly versioned.
+
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Ensure we are not on the `main` branch.
 if [[ "$CURRENT_BRANCH" == "main" ]]; then
-    >&2 echo "Will not replace github references for the main branch. Exiting."
-    exit 1
+  >&2 echo "Will not replace github references for the main branch. Exiting."
+  exit 1
 fi
 
 # Ensure the index is clean
@@ -16,8 +20,12 @@ if ! git diff-index --quiet HEAD --; then
 fi
 
 # Warn if the branch isn't a `release-*` branch (these changes shouldn't be in a branch that gets merged to main)
-if [[ "$CURRENT_BRANCH" != release-* ]]; then
-    >&2 echo "WARNING: doesn't look like a release branch. Continuing anyway."
+if [[ "$CURRENT_BRANCH" == release-* ]]; then
+  STACKABLE_RELEASE="${CURRENT_BRANCH#release-}"
+  echo "Updating stackableRelease to $STACKABLE_RELEASE"
+  sed -i -E "s/(stackableRelease:\s+)(\S+)/\1${STACKABLE_RELEASE}/" stacks/stacks-v2.yaml
+else
+  >&2 echo "WARNING: doesn't look like a release branch. Will not update stackableRelease versions in stacks."
 fi
 
 # Search for githubusercontent urls and replace the branch reference with a placeholder variable
